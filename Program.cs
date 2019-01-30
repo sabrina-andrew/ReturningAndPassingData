16e6ccda007235f3e99ec450f92e1a06382191d8
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-30 18:06:53 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReturningAndPassingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            result = numbersAdded(2, 3);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        static int numbersAdded(int x, int y)
+        {
+            int z = x + y;
+
+            return z;
+        }
+    }
+}
